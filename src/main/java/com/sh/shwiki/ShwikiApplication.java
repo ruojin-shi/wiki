02b9632b4c9875fd0ff855ec5bc697d3e27ec007
Shihuan <Shihuan@qq.com>
2022-12-05 21:56:16 +0800

    创建文件

---
+++ src/main/java/com/sh/shwiki/ShwikiApplication.java
@@ -0,0 +1,13 @@
+package com.sh.shwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShwikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShwikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sh/shwiki/ShwikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.shwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShwikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
